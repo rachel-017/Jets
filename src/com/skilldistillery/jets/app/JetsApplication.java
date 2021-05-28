24413dad75b2512382a6111dff09ce8a2012e57c
ren09 <rachel.richardson915@gmail.com>
2021-05-28 15:23:27 -0600

    init commit

---
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.jets.app;
+
+import com.skilldistillery.jets.entity.*;
+
+public class JetsApplication {
+	
+	private AirField airField;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
