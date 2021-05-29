796eeb2726353782004fb9df407b0482551539e0
ren09 <rachel.richardson915@gmail.com>
2021-05-29 12:39:31 -0600

    saving work

--- src/com/skilldistillery/jets/entity/AirField.java
+++ src/com/skilldistillery/jets/entity/AirField.java
@@ -1,8 +1,43 @@
 package com.skilldistillery.jets.entity;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 public class AirField {
 	private List<Jet> jets;
+
+	public AirField() {
+	}
+
+	public List<Jet> readJets() {
+		
+		Set<String> jetInfo = new HashSet<>();
+
+		BufferedReader file;
+		try {
+			file = new BufferedReader(new FileReader("jets.txt"));
+			String line;
+			while ((line = file.readLine()) != null) {
+				jetInfo.add(line);
+			}
+			for(String info : jetInfo) {
+				
+				String[] splitInfo = info.split(line);
+			}
 	
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return List<Jet> jets;
+	}
 }
--- src/com/skilldistillery/jets/entity/Jet.java
+++ src/com/skilldistillery/jets/entity/Jet.java
@@ -1,5 +1,63 @@
 package com.skilldistillery.jets.entity;
 
 public abstract class Jet {
+	private String model;
+	private double speed;
+	private int range;
+	private long price;
+
+	public Jet(String model, double speed, int range, long price) {
+		super();
+		this.model = model;
+		this.speed = speed;
+		this.range = range;
+		this.price = price;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public double getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(double speed) {
+		this.speed = speed;
+	}
+
+	public int getRange() {
+		return range;
+	}
+
+	public void setRange(int range) {
+		this.range = range;
+	}
+
+	public long getPrice() {
+		return price;
+	}
+
+	public void setPrice(long price) {
+		this.price = price;
+	}
+	
+	@Override
+	public String toString() {
+		return "Flyer Type: " + model + " Max Speed: " + speed + " Max Range: " + range + " Price: " + price + ".";
+	}
+
+	public void fly() {
+		System.out.println("Flyer Type: " + this.model + " Max Speed: " + this.speed + " Max Range: " + this.range
+				+ " Cost: " + this.price + " Max AirTime:"+(this.range/this.speed));
+	}
+	public double getSpeedInMach(double speed) {
+		double speedInMach = this.speed/767;
+		return speedInMach;
+	}
 
 }
